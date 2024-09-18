bb0863a600ea9874a69ffde13222cb7e53209f6f
byeolrim <128365506+byeolrim@users.noreply.github.com>
2024-09-18 14:12:42 +0900

    Update C프1 2주차 과제.c

--- C프1 2주차 과제.c
+++ C프1 2주차 과제.c
@@ -8,16 +8,13 @@
 int main() {
     int a, b;
 
-    // 사용자로부터 두 개의 정수 입력 받기
     printf("두 개의 정수를 입력하세요: ");
     scanf("%d %d", &a, &b);
 
-    // 비트 연산 수행
     int and_result = a & b;
     int or_result = a | b;
     int xor_result = a ^ b;
 
-    // 결과 출력
     printf("AND 결과: %d\n", and_result);
     printf("OR 결과: %d\n", or_result);
     printf("XOR 결과: %d\n", xor_result);
